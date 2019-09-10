82be5272a3a8697cb36c27bce5fb512a6b02d8fd
Daniel Kreling <dkreling@redhat.com>
2019-09-10 10:22:56 +0200

    [CLOUD-3373] After applying an EAP cumulative patch, overridden modules should be removed from the container

--- modules/7.2.4/install.sh
+++ modules/7.2.4/install.sh
@@ -4,4 +4,5 @@ set -e
 
 SOURCES_DIR=/tmp/artifacts/
 
+export JAVA_OPTS="${JAVA_OPTS} -Dorg.wildfly.patching.jar.invalidation=true"
 $JBOSS_HOME/bin/jboss-cli.sh --command="patch apply $SOURCES_DIR/jboss-eap-7.2.4-patch.zip"
