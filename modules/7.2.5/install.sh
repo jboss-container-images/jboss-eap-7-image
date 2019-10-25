a78582570cb7f1345a96c91ed3b7720427df03e2
Daniel Kreling <dkreling@redhat.com>
2019-10-25 13:28:33 +0200

    [CLOUD-3391] Update to EAP 7.2.5 GA

---
+++ modules/7.2.5/install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -e
+
+SOURCES_DIR=/tmp/artifacts/
+
+export JAVA_OPTS="${JAVA_OPTS} -Dorg.wildfly.patching.jar.invalidation=true"
+$JBOSS_HOME/bin/jboss-cli.sh --command="patch apply $SOURCES_DIR/jboss-eap-7.2.5-patch.zip"
