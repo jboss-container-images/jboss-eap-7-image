a47e5834b426f46fd12d025011a6eeb84be90dd0
Daniel Kreling <dkreling@redhat.com>
2020-04-28 14:54:29 +0200

    [CLOUD-3524] [JBEAP-19359] CVE-2019-10174 (infinispan) and CVE-2018-14371 (mojarra)
    CVE-2019-10174: infinispan: invokeAccessibly method from ReflectionUtil / CVE-2018-14371: mojarra: Path traversal in ResourceManager.java:getLocalePrefix

--- modules/7.2.8/install.sh
+++ modules/7.2.8/install.sh
@@ -6,3 +6,4 @@ SOURCES_DIR=/tmp/artifacts/
 
 export JAVA_OPTS="${JAVA_OPTS} -Dorg.wildfly.patching.jar.invalidation=true"
 $JBOSS_HOME/bin/jboss-cli.sh --command="patch apply $SOURCES_DIR/jboss-eap-7.2.8-patch.zip"
+$JBOSS_HOME/bin/jboss-cli.sh --command="patch apply $SOURCES_DIR/jbeap-19359.zip"
